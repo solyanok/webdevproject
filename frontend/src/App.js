7be65eb19f868e98cd032349076e514513b1fc16
Paweł Kornas <kornasnorge@gmail.com>
2023-10-12 12:51:55 +0200

    upd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,36 @@
-import React, { useState, useEffect} from 'react'
-import './App.css';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Boards from './components/Boards';
-import Navbar from './Navbar'
-import Register from './components/Register'
-import Create from './components/Create'
-import Login from './components/Login'
-import Profile from './components/Profile'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Boards from "./components/Boards";
+import Navbar from "./Navbar";
+import Register from "./components/Register";
+import Create from "./components/Create";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
 import axios from "axios";
-import Private from './components/Private';
+import Private from "./components/Private";
 
-import * as jose from "jose"
-import Home from './components/Home';
-import { URL } from './config';
-
+import * as jose from "jose";
+import Home from "./components/Home";
+import { URL } from "./config";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(null);
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
   const [token, setToken] = useState(JSON.parse(localStorage.getItem("token")));
 
-useEffect(()=>{
-console.log(user)
-},[user])
+  useEffect(() => {
+    console.log(user);
+  }, [user]);
 
   useEffect(() => {
     const verify_token = async () => {
       try {
-
         if (!token) {
           setIsLoggedIn(false);
         } else {
@@ -35,24 +38,24 @@ console.log(user)
           const response = await axios.post(`${URL}/user/verify_token`);
           return response.data.ok ? login(token) : logout();
         }
-        } catch (error) {
-          console.log(error);
-        }
+      } catch (error) {
+        console.log(error);
+      }
     };
-      verify_token();
-    }, [token]);
+    verify_token();
+  }, [token]);
 
-    const login = (token) => {
-      let decodedToken = jose.decodeJwt(token);
+  const login = (token) => {
+    let decodedToken = jose.decodeJwt(token);
     let user = {
       userId: decodedToken.userId,
       username: decodedToken.username,
       email: decodedToken.email,
-      password: decodedToken.password
+      password: decodedToken.password,
     };
     localStorage.setItem("token", JSON.stringify(token));
     localStorage.setItem("user", JSON.stringify(user));
-    setUser(user)
+    setUser(user);
     setIsLoggedIn(true);
   };
 
@@ -64,29 +67,61 @@ console.log(user)
 
   return (
     <div className="App">
-      {isLoggedIn !== null &&  <Router>
-         {isLoggedIn ? (
-          <Navbar isLoggedIn={isLoggedIn} logout={logout} />
-        ) : (
-          <Navbar isLoggedIn={false}/>
-        )}      
-    <Routes>
-    
-    <Route path='/' element={<Home /> } />
-    <Route path="/user/register" element={<Register />} />
-    <Route path='/user/login' element={<Login isLoggedIn={isLoggedIn} login={login} /> } />
-    <Route path='/boards/home' element={isLoggedIn ? <Boards user={user}/> : <Navigate to='/user/login' /> } />
-    <Route path='/boards/add' element={isLoggedIn ? (<Create isLoggedIn={isLoggedIn} login={login} user={user} />) : (<Navigate to='/user/login' />) } />
-    <Route path='/user/update' element={<Private setIsLoggedIn={setIsLoggedIn} logout={logout} />}>
-    <Route path='/user/update' element={ <Profile isLoggedIn={isLoggedIn} login={login} user={user} logout={logout} />  } />
-    </Route>
-      
-
-    </Routes>
-  </Router> }
-     
- 
-
+      {isLoggedIn !== null && (
+        <Router>
+          {isLoggedIn ? (
+            <Navbar isLoggedIn={isLoggedIn} logout={logout} />
+          ) : (
+            <Navbar isLoggedIn={false} />
+          )}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/user/register" element={<Register />} />
+            <Route
+              path="/user/login"
+              element={<Login isLoggedIn={isLoggedIn} login={login} />}
+            />
+            <Route
+              path="/boards/home"
+              element={
+                isLoggedIn ? (
+                  <Boards isLoggedIn={isLoggedIn} user={user} />
+                ) : (
+                  <Navigate to="/user/login" />
+                )
+              }
+            />
+            <Route
+              path="/boards/add"
+              element={
+                isLoggedIn ? (
+                  <Create isLoggedIn={isLoggedIn} login={login} user={user} />
+                ) : (
+                  <Navigate to="/user/login" />
+                )
+              }
+            />
+            <Route
+              path="/user/update"
+              element={
+                <Private setIsLoggedIn={setIsLoggedIn} logout={logout} />
+              }
+            >
+              <Route
+                path="/user/update"
+                element={
+                  <Profile
+                    isLoggedIn={isLoggedIn}
+                    login={login}
+                    user={user}
+                    logout={logout}
+                  />
+                }
+              />
+            </Route>
+          </Routes>
+        </Router>
+      )}
     </div>
   );
 }
--- frontend/src/components/Boards.js
+++ frontend/src/components/Boards.js
@@ -1,151 +1,177 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import TaskModal from './components1/TaskModal'
-import BoardsModal from './components1/BoardsModal';
-import trash from '../assets/2891491.png'
-import add from '../assets/992651.png'
-import edit from '../assets/7398464.png'
-import {URL} from '../config'
+import TaskModal from "./components1/TaskModal";
+import BoardsModal from "./components1/BoardsModal";
+import trash from "../assets/2891491.png";
+import add from "../assets/992651.png";
+import edit from "../assets/7398464.png";
+import { URL } from "../config";
 
-function Boards (user){
-const [boards, setBoards] = useState(
-  [])
-const [open, setIsOpen] = useState(false)
-const[openBoards, setIsOpenBoards] = useState(false)
-  const [tasks, setTasks] = useState([])
-  const [selectedBoard, setSelectedBoard] = useState(null)
- 
+function Boards({ isLoggedIn, user }) {
+  const [boards, setBoards] = useState([]);
+  const [open, setIsOpen] = useState(false);
+  const [openBoards, setIsOpenBoards] = useState(false);
+  const [tasks, setTasks] = useState([]);
+  const [selectedBoard, setSelectedBoard] = useState(null);
 
+  console.log(isLoggedIn);
+  const getBoards = async () => {
+    try {
+      const userId = JSON.parse(localStorage.getItem("user")).userId;
+      const response = await axios.get(`${URL}/boards/home`, {
+        userId: userId,
+      });
+      setBoards(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-    const getBoards = async () =>
-{
-  try {
+  const getTasks = async () => {
+    try {
+      const response = await axios.get(`${URL}/tasks/home`);
+      console.log(response);
+      setTasks(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-    const userId = JSON.parse(localStorage.getItem('user')).userId;
-    const response = await axios.get(`${URL}/boards/home`,  { userId:userId} )
+  useEffect(() => {
+    getBoards();
+  }, []);
 
-setBoards(response.data)
+  useEffect(() => {
+    getTasks();
+  }, []);
 
-  }catch(e) {
-    console.log(e)
-  }
-}
+  const handleClick = (id) => {
+    setIsOpen(true);
+    setSelectedBoard(id);
+  };
 
-const getTasks = async () =>
-{
-  try {
-    const response = await axios.get(`${URL}/tasks/home`)
-    console.log(response);
-setTasks(response.data)
+  const handleClickBoard = (id) => {
+    setIsOpenBoards(true);
+    setSelectedBoard(id);
+  };
 
-  }catch(e) {
-    console.log(e)
-  }
-}
+  const addTask = (newTask) => {
+    setTasks((tasks) => [...tasks, newTask]);
+    setIsOpen(false);
+  };
 
-
-
-useEffect(()=>{
-    getBoards()
-}, [])
-useEffect(()=>{
-    getTasks()
-}, [])
-
-const handleClick =(id) => {
-    setIsOpen(true)
-setSelectedBoard(id)
-}
-
-const handleClickBoard =(id) => {
-    setIsOpenBoards(true)
-setSelectedBoard(id)
-}
-
-
-
-const addTask = (newTask) => {
-setTasks((tasks) => [...tasks, newTask]);
-setIsOpen(false);}
-
-
-const handleDeleteBoard = async (id) => {
-    if (window.confirm('Are you sure you want to delete this board?')) {
+  const handleDeleteBoard = async (id) => {
+    if (window.confirm("Are you sure you want to delete this board?")) {
       try {
-        const response = await axios.delete(`${URL}/boards/delete`, { data: {boardId: id} }); 
-      if (response.data.ok){
-        setBoards((boards) => boards.filter((e) => e._id !== id))}
+        const response = await axios.delete(`${URL}/boards/delete`, {
+          data: { boardId: id },
+        });
+        if (response.data.ok) {
+          setBoards((boards) => boards.filter((e) => e._id !== id));
+        }
       } catch (e) {
         console.log(e);
       }
     }
-  }
+  };
   const handleDeleteTask = async (id) => {
-    if (window.confirm('Are you sure you want to delete this task?')) {
+    if (window.confirm("Are you sure you want to delete this task?")) {
       try {
-        const response = await axios.delete(`${URL}/tasks/delete`, { data: {taskId: id} }); 
-      if (response.data.ok){
-        setTasks((tasks) => tasks.filter((e) => e._id !== id))}
+        const response = await axios.delete(`${URL}/tasks/delete`, {
+          data: { taskId: id },
+        });
+        if (response.data.ok) {
+          setTasks((tasks) => tasks.filter((e) => e._id !== id));
+        }
       } catch (e) {
         console.log(e);
       }
     }
-  }
+  };
 
   const toggleTaskCompleted = async (taskId, completed) => {
     try {
-      const response = await axios.post(`${URL}/tasks/complete`, { taskId, completed });
+      const response = await axios.post(`${URL}/tasks/complete`, {
+        taskId,
+        completed,
+      });
       if (response.data.ok) {
-        setTasks((tasks) => tasks.map((task) => {
-          if (task._id === taskId) {
-            return { ...task, completed };
-          }
-          return task;
-        }));
+        setTasks((tasks) =>
+          tasks.map((task) => {
+            if (task._id === taskId) {
+              return { ...task, completed };
+            }
+            return task;
+          })
+        );
       }
     } catch (e) {
       console.log(e);
     }
-  }
+  };
 
-return (
+  return (
     <div className="boards-container">
       {boards.length > 0 &&
         boards.map((board) => (
           <div className="board">
-            <div className='board-wrapper'>
-            <div className='board-text'>
-            <h2>{board.name}</h2>
-            <p>{board.description}</p>
+            <div className="board-wrapper">
+              <div className="board-text">
+                <h2>{board.name}</h2>
+                <p>{board.description}</p>
+              </div>
+              <div className="icons">
+                <button
+                  type="button"
+                  onClick={() => handleClickBoard(board._id)}
+                >
+                  <img className="icon" src={edit} alt="edit"></img>
+                </button>
+                <button
+                  type="button"
+                  onClick={() => handleDeleteBoard(board._id)}
+                >
+                  {" "}
+                  <img className="icon" src={trash} alt="trash"></img>{" "}
+                </button>
+              </div>
             </div>
-            <div className='icons'>
-            <button type="button" onClick={()=>handleClickBoard(board._id)}><img className='icon' src={edit} alt='edit'></img></button>
-            <button type="button" onClick={()=> handleDeleteBoard(board._id)} > <img className='icon' src={trash} alt='trash'></img> </button>
-           
+            <div className="icon-add">
+              <button type="button" onClick={() => handleClick(board._id)}>
+                <img className="icon" src={add} alt="add"></img>
+              </button>
             </div>
-            </div>
-            <div className='icon-add'>
-                   <button type="button" onClick={() => handleClick(board._id)}>
-            <img className='icon' src={add} alt='add'></img>
-            </button>
-                      
-                      </div>
-            <div className='tasks-container'>
+            <div className="tasks-container">
               {tasks.length > 0 &&
                 tasks.map((task) =>
                   task.boards_id === board._id ? (
-                    <div className={`task ${task.completed ? 'completed' : ''}`}>
-                        
-                        <div className='task-text'>
-                      <h3>{task.name}</h3>
-                      <p>{task.description}</p>
-                      <p>
-                          Due Date: {new Date(task.dueDate).toLocaleDateString()}
-                        </p><button className='icon1' type="button" onClick={()=> handleDeleteTask(task._id)}><img className='icon' src={trash} alt='trash'></img></button>
-                        </div>
-                    
-                      <button type="button" className='complete-btn' onClick={() => toggleTaskCompleted(task._id, !task.completed)}>
-                        {task.completed ? 'Mark Incomplete' : 'Mark Complete'}
+                    <div
+                      className={`task ${task.completed ? "completed" : ""}`}
+                    >
+                      <div className="task-text">
+                        <h3>{task.name}</h3>
+                        <p>{task.description}</p>
+                        <p>
+                          Due Date:{" "}
+                          {new Date(task.dueDate).toLocaleDateString()}
+                        </p>
+                        <button
+                          className="icon1"
+                          type="button"
+                          onClick={() => handleDeleteTask(task._id)}
+                        >
+                          <img className="icon" src={trash} alt="trash"></img>
+                        </button>
+                      </div>
+
+                      <button
+                        type="button"
+                        className="complete-btn"
+                        onClick={() =>
+                          toggleTaskCompleted(task._id, !task.completed)
+                        }
+                      >
+                        {task.completed ? "Mark Incomplete" : "Mark Complete"}
                       </button>
                     </div>
                   ) : null
@@ -153,10 +179,24 @@ return (
             </div>
           </div>
         ))}
-    {selectedBoard &&  <TaskModal open={open} setIsOpen={setIsOpen} board={selectedBoard} addTask={addTask}  />}
-    {selectedBoard &&  <BoardsModal openBoards={openBoards} setIsOpenBoards={setIsOpenBoards} board={selectedBoard} setBoards={setBoards} user={user}   />}
+      {selectedBoard && (
+        <TaskModal
+          open={open}
+          setIsOpen={setIsOpen}
+          board={selectedBoard}
+          addTask={addTask}
+        />
+      )}
+      {selectedBoard && (
+        <BoardsModal
+          openBoards={openBoards}
+          setIsOpenBoards={setIsOpenBoards}
+          board={selectedBoard}
+          setBoards={setBoards}
+          user={user}
+        />
+      )}
     </div>
   );
-
 }
-export default Boards
+export default Boards;
